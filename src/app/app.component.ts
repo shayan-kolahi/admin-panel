241c172c78781b10445397c385e2fd2c77b3a9a6
shayan kolahi <shayankolahi30@gmail.com>
2022-11-24 17:24:15 +0330

    make logOut link and event keyup in auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,9 @@
 import {Component, OnInit} from '@angular/core';
-import { Router } from '@angular/router';
-import {CookieService} from "ngx-cookie-service";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit {
-  constructor(private router: Router,
-              private cookieService: CookieService,) { }
-
-  ngOnInit(): void {
-
-  }
+  constructor() {}
+  ngOnInit(): void {}
 }
--- src/app/panel/component/header/header.component.ts
+++ src/app/panel/component/header/header.component.ts
@@ -1,23 +1,42 @@
-import { Component, OnInit } from '@angular/core';
-import { Output, EventEmitter } from '@angular/core';
+import { Output, EventEmitter , Component } from '@angular/core';
+import {CookieService} from "ngx-cookie-service";
+import {Router} from "@angular/router";
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class HeaderComponent {
+  constructor(private cookieService: CookieService,
+              private router: Router,) { }
   @Output() status_sidebar = new EventEmitter<boolean>();
-
   isActive : boolean = true;
   click_status() {
     this.isActive = !this.isActive
     this.status_sidebar.emit(this.isActive);
   }
+  logOut(){
+    Swal.fire({
+      customClass: {
+        confirmButton: 'btn btn-link ml-3',
+        cancelButton: 'btn btn-primary'
+      },
+      buttonsStyling: false,
+      title: 'مطمئنی',
+      text: "میخوای خارج شی ؟",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'بله',
+      cancelButtonText: 'نه بازگشت',
+      reverseButtons: true
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.cookieService.delete('token');
+        this.router.navigate(['/auth'])
+      }
+    })
 
+  }
 }
