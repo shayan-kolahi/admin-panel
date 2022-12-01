ac7354752d62743251b909dd8ee166d0d8946ef2
shayan kolahi <shayankolahi30@gmail.com>
2022-12-01 22:05:02 +0330

    delete currency.component.scss

--- src/app/currency/component/currency/currency.component.ts
+++ src/app/currency/component/currency/currency.component.ts
@@ -4,7 +4,6 @@ import {CurrencyService} from "../../../services/currency.service";
 @Component({
   selector: 'app-currency',
   templateUrl: './currency.component.html',
-  styleUrls: ['./currency.component.scss']
 })
 export class CurrencyComponent implements OnInit {
 
